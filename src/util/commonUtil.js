3ca5aab873df894df296a5d4bbd42dd1e0f71ecc
SeonHyungJo <seonhyung.jo@gmail.com>
2019-01-01 20:45:32 +0900

    :wrench: 공통작업 / multiClass추가 기능 추가

---
+++ src/util/commonUtil.js
@@ -0,0 +1,9 @@
+
+export function classNames(classList) {
+    if(classList === null || classList === undefined){
+        return '';
+    }
+
+    const textList = classList.split(', ')
+    return textList.join(' ');
+}
