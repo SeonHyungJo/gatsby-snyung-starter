5b8dc7631398f8d1598460f798df914a6d68cb52
SeonHyungJo <seonhyung.jo@gmail.com>
2019-01-06 17:46:11 +0900

    :wrench: Main page / Main Page 완성

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,12 +6,17 @@ import '../css/button.scss';
 /**
  * @description 상단 헤더 부분 Nav Button 구현
  */
-const Button = ({ children = "Button", type = "nav" }) => {
+const Button = ({ children = "Button", type = "nav", to = "" }) => {
     return (
-        <div className={type.toLowerCase() === "nav" ? classNames(`defaultClass, buttonContainer`) : classNames(`defaultClass, ${type}`)}>
-            {children}
-        </div >
+        <a href={to}>
+            <div className={type.toLowerCase() === "nav" ? classNames(`defaultClass, buttonContainer`) : classNames(`defaultClass, ${type}`)} >
+                {children}
+            </div>
+        </a>
     );
+}
+
+const movePage = (e) => {
 }
 
 export default Button;
