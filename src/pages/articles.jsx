dce3dcb54780fd0f3feaa83a7d4191ae3d034661
SeonHyungJo <seonhyung.jo@gmail.com>
2019-02-06 01:09:51 +0900

    :wrench: Code Refactoring / M article page

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -1,23 +1,82 @@
 import React from 'react'
+import GatsbyLink from 'gatsby-link'
 import { graphql } from 'gatsby'
 import PropTypes from 'prop-types'
 
 import Layout from 'component/Layout'
-import PostList from 'component/PostList'
+import { LinkButton } from 'component/ButtonComponent'
+import { classNames } from 'util/commonUtil'
 
 const Article = props => {
-  const { data } = props
+  const { data, pageContext } = props
   const { edges: posts } = data.allMarkdownRemark
+  const { next, prev, numPages } = pageContext
 
   return (
     <Layout {...props}>
-      <PostList posts={posts} />
+      <div className="blog-posts">
+        {posts
+          .filter(post => post.node.frontmatter.title.length > 0)
+          .map(({ node: post }) => {
+            return (
+              <div className="blog-post-preview" key={post.id}>
+                <div className="blog-container">
+                  <p className="title">
+                    <GatsbyLink to={post.frontmatter.path}>
+                      {post.frontmatter.title}
+                    </GatsbyLink>
+                  </p>
+                </div>
+                <div className="blog-container">
+                  <div className="blog-sub-container">
+                    <p className="summary">{post.excerpt}</p>
+                    <div className="tagContainer">
+                      {post.frontmatter.tags.map(tag => {
+                        return (
+                          <GatsbyLink
+                            key={`postList_${tag}`}
+                            to={`/category/${tag}`}
+                          >
+                            <span className="tag">{tag}</span>
+                          </GatsbyLink>
+                        )
+                      })}
+                    </div>
+                  </div>
+                  <div className={classNames('blog-sub-container, right')}>
+                    <p className="date">{post.frontmatter.date}</p>
+                    <p className="author">{`By ${post.frontmatter.author}`}</p>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        <div className="posts-bottom">
+          {prev === 0 ? (
+            <div />
+          ) : (
+            <LinkButton to={`/posts/${prev}`} customClass={'moveLink'}>
+              {' '}
+              ← Prev
+            </LinkButton>
+          )}
+          {next - 1 === numPages ? (
+            <div />
+          ) : (
+            <LinkButton to={`/posts/${next}`} customClass={'moveLink'}>
+              {' '}
+              Next →
+            </LinkButton>
+          )}
+        </div>
+      </div>
     </Layout>
   )
 }
 
 Article.propTypes = {
-  data: PropTypes.object
+  data: PropTypes.object,
+  pageContext: PropTypes.object
 }
 
 export const ArticleQuery = graphql`
