54ad94771d9eafa6e22e31d132419fc7003bc374
SeonHyungJo <seonhyung.jo@gmail.com>
2019-12-03 01:34:46 +0900

    :wrench: PWA제거

--- gatsby-config.js
+++ gatsby-config.js
@@ -129,7 +129,7 @@ module.exports = {
         }
       }
     },
-    'gatsby-plugin-offline',
+    // 'gatsby-plugin-offline',
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
